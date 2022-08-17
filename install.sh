e6d363b2842bae879d022f08bc7c908231fb4285
Pablo Emidio <p.emidiodev@gmail.com>
2022-08-17 01:35:04 -0300

    Add install file

---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+
+PROJECT_DIR_NAME=".iproject"
+
+install_project()
+{
+   # Create project dir
+   PROJECT_DIR=$HOME/$PROJECT_DIR_NAME
+   mkdir -p $PROJECT_DIR
+
+   # Copy project files to project dir
+   CURRENT_DIR=$(dirname "$0")
+   chmod +x $CURRENT_DIR/iproject/bin/iproject
+   cp -r $CURRENT_DIR/iproject/* $PROJECT_DIR/
+}
+
+
+
+export_bin_dir()
+{
+   # Export bin dir
+   BASH_SHELL_CONFIG_FILE=$HOME/.bashrc
+   ZSH_SHELL_CONFIG_FILE=$HOME/.zshrc
+
+   if [ $(grep -c $PROJECT_DIR_NAME/bin $BASH_SHELL_CONFIG_FILE) -eq 0 ]; then
+      echo "export PATH=\"\$HOME/$PROJECT_DIR_NAME/bin:\$PATH\"" >> $BASH_SHELL_CONFIG_FILE
+   fi
+   
+   if [ $(grep -c $PROJECT_DIR_NAME/bin $ZSH_SHELL_CONFIG_FILE) -eq 0 ]; then
+      echo "export PATH=\"\$HOME/$PROJECT_DIR_NAME/bin:\$PATH\"" >> $ZSH_SHELL_CONFIG_FILE
+   fi
+   
+}
+
+
+install_project
+export_bin_dir
